fbd6eeec5d02dc0a3fe7dcfea170338e4c5a080b
Lucas Daniel Batista <luksdanielbatista2@gmail.com>
2026-03-30 21:57:10 -0300

    Fixed project name and references

--- src/main/java/com/lucasbatista/projects/WeddingGiftListApiApplication.java
+++ src/main/java/com/lucasbatista/projects/WeddingGiftListApiApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class MarriageGiftListApiApplication {
+public class WeddingGiftListApiApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(MarriageGiftListApiApplication.class, args);
+		SpringApplication.run(WeddingGiftListApiApplication.class, args);
 	}
 
 }
